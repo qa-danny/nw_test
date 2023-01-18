ead8381b8dd163358908ee6f60c74f171719137d
Daniel Schaich <dschaich@zappos.com>
2023-01-18 09:27:49 -0800

    added git ignore, small changes

--- e2e/test/expectedResponseTest.js
+++ e2e/test/expectedResponseTest.js
@@ -3,14 +3,10 @@ const fetch = require('node-fetch');
 module.exports = {
   before: async function(browser) {
     browser.globals.waitForConditionTimeout = 10000;
-    await browser.navigateTo('https://www.zappos.com/cart');
+    await browser.navigateTo('https://www.google.com/');
   },
   'API Test - GET': async function(browser) {
-    await browser.expect.url().to.endWith('/cart');
     const apiUrl = 'https://dog.ceo/api/breeds/list/all';
-    // const response = await fetch(apiUrl);
-    // // const data = await response.json();
-    // console.log(response);
     await browser.waitForExpectedResponse(() => fetch(apiUrl), { dogBreeds: 2 });
     console.log('completed Wait For expected Response. Time to pause');
     await browser.pause(200);
