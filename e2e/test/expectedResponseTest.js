1cd6b28cf7a6ba09fc6cd1ab3154abdd8a329584
Daniel Schaich <dschaich@zappos.com>
2023-01-18 09:35:46 -0800

    another small update removing pauses and other breaks, update to gitignore file

--- e2e/test/expectedResponseTest.js
+++ e2e/test/expectedResponseTest.js
@@ -9,9 +9,6 @@ module.exports = {
     const apiUrl = 'https://dog.ceo/api/breeds/list/all';
     await browser.waitForExpectedResponse(() => fetch(apiUrl), { dogBreeds: 2 });
     console.log('completed Wait For expected Response. Time to pause');
-    await browser.pause(200);
-    console.log('completed pause. do it again');
-    await browser.pause(500);
   },
   after: async function(browser) {
     browser.end();
