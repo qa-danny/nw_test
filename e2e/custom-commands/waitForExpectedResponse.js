e25c4d6a73564208357cbbeb8a546b5b7288b879
Daniel Schaich <dschaich@zappos.com>
2023-01-18 09:13:31 -0800

    other updates, new packages, test running

--- e2e/custom-commands/waitForExpectedResponse.js
+++ e2e/custom-commands/waitForExpectedResponse.js
@@ -1,40 +1,36 @@
-// import { deepEqual } from 'fast-equals';
+const deepEqual = require('deep-equal')
+/**
+ * Wait for API to come back with expected properties in the response
+ * Will retry an API, and has a timeout between each call
+ * @param {Function} apiMethod      API method you want to fire and retry
+ * @param {Object} expectedResponse An object of properties you'd expect to find in the response
+ * @param {Number} [retries=3]      Number of retries the API should run before failing
+ * @param {Number} [timeout=1000]   Time in milliseconds of how long to wait between each retry
+ */
+module.exports = class WaitForExpectedResponse {
+  async command(apiMethod, expectedResponse, retries = 3, timeout = 1500) {
+    let response;
+    let doesContainExpected;
+    for (; retries > 0; retries--) {
+      console.log('for loop', new Date());
+      try {
+        response = await apiMethod();
+        console.log('completed apiMethod()', new Date());
+      } catch (error) {
+        await this.api.assert.ok(false, error);
+      }
+      const data = response.json();
+      // look through all keys in expectedResponse, and check the API response to see if they deepEqual
+      doesContainExpected = Object.keys(expectedResponse).every(key => deepEqual(expectedResponse[key], data[key]));
+      console.log(doesContainExpected);
+      if (doesContainExpected) {
+        return;
+      }
+        console.log('pausing in function', new Date());
+        await this.api.pause(timeout);
+        console.log('about to repeat loop', new Date());
+    }
+    await this.api.assert.ok(doesContainExpected, `Exceeded max number of retries. API Response ${JSON.stringify(response)} does not contain Expected Response ${JSON.stringify(expectedResponse)}`);
+  }
+};
 
-// /**
-//  * Wait for API to come back with expected properties in the response
-//  * Will retry an API, and has a timeout between each call
-//  * @param {Function} apiMethod      API method you want to fire and retry
-//  * @param {Object} expectedResponse An object of properties you'd expect to find in the response
-//  * @param {Number} [retries=3]      Number of retries the API should run before failing
-//  * @param {Number} [timeout=1000]   Time in milliseconds of how long to wait between each retry
-//  */
-// module.exports = class WaitForExpectedResponse {
-//   async command(expectedResponse, retries = 3, timeout = 1500) {
-//     let response;
-//     let doesContainExpected;
-//     for (; retries > 0; retries--) {
-//       console.log('for loop', new Date());
-//       try {
-//         response = await catBreed(2);
-//         console.log(response, new Date());
-//         response = await response;
-//       } catch (error) {
-//         await this.api.assert.ok(false, error);
-//       }
-//       // look through all keys in expectedResponse, and check the API response to see if they deepEqual
-//       doesContainExpected = Object.keys(expectedResponse).every(key => deepEqual(expectedResponse[key], response[key]));
-//       console.log(doesContainExpected);
-//       if (doesContainExpected) {
-//         return;
-//       }
-//         console.log('pausing in function', new Date());
-//         await this.api.pause(timeout);
-//         console.log('about to repeat loop', new Date());
-//     }
-//     await this.api.assert.ok(doesContainExpected, `Exceeded max number of retries. API Response ${JSON.stringify(response)} does not contain Expected Response ${JSON.stringify(expectedResponse)}`);
-//   }
-// };
-
-// async function catBreed(limit = 1, fetcher = timedFetch('getCatBreeds')) {
-//   return fetcher(`https://catfact.ninja/breeds/limit=${limit}`);
-// }
--- e2e/test/expectedResponseTest.js
+++ e2e/test/expectedResponseTest.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 module.exports = {
   before: async function(browser) {
     browser.globals.waitForConditionTimeout = 10000;
@@ -5,23 +7,17 @@ module.exports = {
   },
   'API Test - GET': async function(browser) {
     await browser.expect.url().to.endWith('/cart');
-    let apiUrl = 'https://dog.ceo/api/breeds/list/all';
-    console.log(apiUrl);
-    let response = await browser.apiGet(apiUrl);
-    console.log(JSON.stringify(response));
-    const newData = JSON.stringify(response);
-    await browser.assert.includes(newData, 'auth');
+    const apiUrl = 'https://dog.ceo/api/breeds/list/all';
+    // const response = await fetch(apiUrl);
+    // // const data = await response.json();
+    // console.log(response);
+    await browser.waitForExpectedResponse(() => fetch(apiUrl), { dogBreeds: 2 });
+    console.log('completed Wait For expected Response. Time to pause');
+    await browser.pause(200);
+    console.log('completed pause. do it again');
+    await browser.pause(500);
   },
   after: async function(browser) {
     browser.end();
   }
 }
-// describe('expected response example', async function() {
-//   before(browser => browser.navigateTo('https://www.zappos.com/cart'));
-//   it('Checks an expected response', async function(browser) {
-//     const cookies = await browser.getCookies();
-//     console.log(cookies);
-//     const response = await browser.waitForExpectedResponse('test');
-//     console.log(response);
-//   });
-// });
